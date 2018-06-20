f5a892946db4c2b8dcd5f8e9059c1730428e6a86
Davide Lasagna <lasagnadavide@gmail.com>
2018-06-20 18:28:16 +0100

    remove variational code form bench script

--- tests/bench.jl
+++ tests/bench.jl
@@ -2,21 +2,15 @@ using KS
 
 L = 1.0
 
-@printf " n  -  t_forw [μs] - t_coupled [μs] - ratio\n"
-for n = 10:100
+@printf " n  -  t_forw [μs]\n"
+for n = (1000-30):(1000+30)
 	# define right hand side
-    F  = KSEq(n, L, 0, false, :forward)
-    FL = KSEq(n, L, 0, false, :tangent)
+    F  = KSEq(n, L, 0, false)
 
     # benchmark forward code
     U, V = FTField(n, L, false), FTField(n, L, false)
     F(0.0, U, V) 
-    tF = minimum([@elapsed F(0.0, U, V) for i = 1:5000])
+    t = minimum([@elapsed F(0.0, U, V) for i = 1:5000])
 
-    # benchmark forward+tangent code
-    U, V = VarFTField(n, L, false), VarFTField(n, L, false)
-    FL(0.0, U, V) 
-    tFL = minimum([@elapsed FL(0.0, U, V) for i = 1:5000])
-
-    @printf "%03d - %7.4f      - %9.4f      - %5.3f\n" n tF*10^6 tFL*10^6 tFL/tF
+    @printf "%03d - %7.4f\n" n t*10^6
 end
