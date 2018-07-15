5a0650b01cb9bc53bd45b6d801e7772fa75c63fa
Davide Lasagna <lasagnadavide@gmail.com>
2018-07-15 13:42:46 +0100

    added test for adjoint code

--- tests/test_linearised.jl
+++ tests/test_linearised.jl
@@ -5,7 +5,7 @@ using Flows
 @testset "test adjoint identities" begin
     # parameters
     ν        = (2π/100)^2
-    Δt       = 0.5*ν
+    Δt       = 0.01*ν
     T        = 100*ν
     rkmethod = :CB4_4R3R
     ISODD    = false
@@ -34,21 +34,30 @@ using Flows
     L  = LinearisedEquation(n, ν, ISODD, KS.TangentMode(), sol)
     L⁺ = LinearisedEquation(n, ν, ISODD, KS.AdjointMode(), sol)
 
-    # integrators
-    ψ  = integrator(splitexim(L)..., 
-                   Scheme(rkmethod, FTField(n, ISODD)), TimeStepConstant(Δt))
-    ψ⁺ = integrator(splitexim(L⁺)..., 
-                   Scheme(rkmethod, FTField(n, ISODD)), TimeStepConstant(Δt));
-
     # will use these random field for testing
     V  = FTField(n, ISODD, k->exp(2π*im*rand())/k)
     V⁺ = FTField(n, ISODD, k->exp(2π*im*rand())/k);
 
     # test that ⟨V, ℒ⁺[V⁺]⟩ + ⟨V⁺, ℒ[V]⟩ = 0 to machine accuracy
-    v1 = dot(copy(V),  L⁺(0, U, U, V⁺, FTField(n, ISODD)))
-    v2 = dot(copy(V⁺), L( 0, U, U, V,  FTField(n, ISODD)))
+    v1 = dot(V,  L⁺(0, U, U, V⁺, FTField(n, ISODD)))
+    v2 = dot(V⁺, L( 0, U, U, V,  FTField(n, ISODD)))
 
     @test abs(v1+v2) < 2*eps(v1)*n
+
+    # test that the difference ⟨V, ψ⁺[V⁺]⟩ - ⟨V⁺, ψ[V]⟩
+    # goes to zero as we refine the time step, since
+    # the tangent and adjoint integrators are not
+    # discretely consistent with each other.
+    for Δt in ν.*[1, 0.5, 0.1, 0.05, 0.01, 0.005, 0.001, 0.0005]
+        # integrators
+        ψ  = integrator(splitexim(L)..., 
+                       Scheme(rkmethod, FTField(n, ISODD)), TimeStepConstant(Δt))
+        ψ⁺ = integrator(splitexim(L⁺)..., 
+                       Scheme(rkmethod, FTField(n, ISODD)), TimeStepConstant(Δt))
+        v1 = dot(V, ψ⁺(copy(V⁺), (100*ν, 99*ν)))
+        v2 = dot(V⁺, ψ(copy(V),  (99*ν, 100*ν)))
+        @test abs(v1-v2)/Δt^4 < 10^9
+    end
 end
 
 # @testset "perturbations to u(x) = 0 decays exponentially" begin
