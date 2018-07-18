3cac317599126120963604a9d3f0a6b180cb8367
Davide Lasagna <lasagnadavide@gmail.com>
2018-07-18 17:44:24 +0100

    added test for linearised equations

--- tests/test_linearised.jl
+++ tests/test_linearised.jl
@@ -5,9 +5,9 @@ using Flows
 @testset "test adjoint identities                " begin
     # parameters
     ν        = (2π/100)^2
-    Δt       = 0.01*ν
+    Δt       = 1e-4*ν
     T        = 100*ν
-    rkmethod = :CB4_4R3R
+    rkmethod = :CB3e_3R2R
     ISODD    = false
     n        = 100
 
@@ -48,7 +48,7 @@ using Flows
     # goes to zero as we refine the time step, since
     # the tangent and adjoint integrators are not
     # discretely consistent with each other.
-    for Δt in ν.*[1, 0.5, 0.1, 0.05, 0.01, 0.005, 0.001, 0.0005]
+    for Δt in ν.*logspace(-1, -4, 10)
         # integrators
         ψ  = integrator(splitexim(L)..., 
                        Scheme(rkmethod, FTField(n, ISODD)), TimeStepConstant(Δt))
@@ -56,7 +56,7 @@ using Flows
                        Scheme(rkmethod, FTField(n, ISODD)), TimeStepConstant(Δt))
         v1 = dot(V, ψ⁺(copy(V⁺), (100*ν, 99*ν)))
         v2 = dot(V⁺, ψ(copy(V),  (99*ν, 100*ν)))
-        @test abs(v1-v2)/Δt^4 < 10^9
+        @test abs(v1-v2)/Δt^3 < 4.5*10^6
     end
 end
 
@@ -66,7 +66,7 @@ end
     ν        = (2π/10)^2
     Δt       = 0.01*ν
     T        = 1*ν
-    rkmethod = :CB4_4R3R
+    rkmethod = :CB3e_3R2R
     ISODD    = false
     n        = 15
 
@@ -108,7 +108,7 @@ end
         ψ(V, (0, T))
 
         # check match with exact solution
-        @test abs(V[k] - exp( T*(k^2 - ν*k^4) )) < 1e-10
+        @test abs(V[k] - exp( T*(k^2 - ν*k^4) )) < 1e-7
         
         # ADJOINT
         # set terminal condition to be proportional to cos(2π/L⋅k⋅x)
@@ -118,6 +118,61 @@ end
         ψ⁺(V, (T, 0))
 
         # check match with exact solution
-        @test abs(V[k] - exp(T*(k^2 - ν*k^4) )) < 1e-10
+        @test abs(V[k] - exp(T*(k^2 - ν*k^4) )) < 1e-7
     end
+end
+
+@testset "perturbations to nonlinear trajectory  " begin
+    # parameters
+    ν        = (2π/22)^2
+    Δt       = 1e-1*ν
+    T        = 1*ν
+    rkmethod = :CB3e_3R2R
+    ISODD    = false
+    n        = 22
+
+    # seed rng
+    srand(0)
+
+    # system right hand side
+    F = ForwardEquation(n, ν, ISODD)
+
+    # integrator
+    ϕ = integrator(splitexim(F)..., 
+                   Scheme(rkmethod, FTField(n, ISODD)), TimeStepConstant(Δt))
+
+    # monitor to store the forward solution
+    sol = Monitor(FTField(n, ISODD), copy)
+
+    # land on attractor
+    U₀ = ϕ(FTField(n, ISODD, k->exp(2π*im*rand())/k), (0, 100*ν))
+
+    # rhs
+    L  = LinearisedEquation(n, ν, ISODD, KS.TangentMode(), sol)
+
+    # integrators
+    ψ  = integrator(splitexim(L)..., 
+                   Scheme(rkmethod, FTField(n, ISODD)), TimeStepConstant(Δt))
+
+    # copy initial condition
+    _U₀ = copy(U₀); 
+
+    # and get final point
+    Uₜ0 = ϕ(copy(_U₀), (0, T), reset!(sol))
+
+    # copy initial condition and perturb it
+    _U₀ = copy(U₀); _U₀[1] += 1e-6
+
+    # and get final point
+    Uₜ1 = ϕ(copy(_U₀), (0, T))
+
+    # Now use the direct method, i.e. calculate the direction derivative along a particular perturbation
+    V  = FTField(n, ISODD, k->0); V[1] = 1
+
+    # integrate direct equations, then apply shift
+    ψ(V, (0, T))
+
+    # cannot demand too much from finite differences
+    @test abs(V[1] - 1e6*(Uₜ1 - Uₜ0)[1]) < 3e-6
+    @test abs(V[2] - 1e6*(Uₜ1 - Uₜ0)[2]) < 3e-6
 end
