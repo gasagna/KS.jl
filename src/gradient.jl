38c35c236b21bdf7b855b4a43283b1783af1122b
Davide Lasagna <lasagnadavide@gmail.com>
2018-07-24 12:04:43 +0100

    generalised asis interface and added tests

--- src/gradient.jl
+++ src/gradient.jl
@@ -4,30 +4,32 @@ import asis: tovector!,
 			 _checksize
 
 # IO functions for gradient calculations
-function tovector!(out::Vector, ∇ᵤJ::FTField{n}, ∇ₜJ::Real, ∇ₛJ::Real) where {n}
+function tovector!(out::Vector, ∇ᵤJ::NTuple{N, FTField{n}}, ∇ₜJ::Real, ∇ₛJ::Real) where {N, n}
     _checksize(out, ∇ᵤJ)
-    @inbounds @simd for i in eachindex(out)
-        out[i] = ∇ᵤJ[i]
+    idx = 1
+    for i = 1:N
+        @inbounds @simd for el in ∇ᵤJ[i]
+            out[idx] = el
+            idx += 1
+        end
     end
     out[end-1] = ∇ₜJ
     out[end]   = ∇ₛJ
     return out
 end
 
-function tovector(∇ᵤJ::FTField{n}, ∇ₜJ::Real, ∇ₛJ::Real) where {n}
-	out = zeros(length(∇ᵤJ) + 2)
-    @inbounds @simd for i in eachindex(out)
-        out[i] = ∇ᵤJ[i]
-    end
-    out[end-1] = ∇ₜJ
-    out[end]   = ∇ₛJ
-    return out
-end
+tovector(∇ᵤJ::NTuple{N, FTField{n}}, ∇ₜJ::Real, ∇ₛJ::Real) where {N, n} = 
+	tovector!(zeros(sum(length.(∇ᵤJ)) + 2), ∇ᵤJ, ∇ₜJ, ∇ₛJ)
 
-function fromvector!(∇ᵤJ::FTField{n}, out::Vector) where {n}
+function fromvector!(∇ᵤJ::NTuple{N, FTField{n}}, out::Vector) where {N, n}
     _checksize(out, ∇ᵤJ)
-    @inbounds @simd for i in eachindex(out)
-        ∇ᵤJ[i] = out[i]
+    idx = 1
+    for i = 1:N
+        ∇ᵤJᵢ = ∇ᵤJ[i] # alias
+        @inbounds @simd for j in 1:length(∇ᵤJᵢ)
+            ∇ᵤJᵢ[j] = out[idx]
+            idx += 1
+        end
     end
     return ∇ᵤJ, out[end-1], out[end]
 end
