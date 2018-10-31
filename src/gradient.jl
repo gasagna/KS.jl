09e3302115d1c2d8595d0fa72f04624b87a160d3
Davide Lasagna <lasagnadavide@gmail.com>
2018-10-31 09:52:49 +0000

    use new api

--- src/gradient.jl
+++ src/gradient.jl
@@ -33,3 +33,41 @@ function fromvector!(∇ᵤJ::NTuple{N, FTField{n}}, out::Vector) where {N, n}
     end
     return ∇ᵤJ, out[end-1], out[end]
 end
+
+# ---------------------------------------------------------------------------- #
+# IO functions for gradient calculations with new API   
+function tovector!(out::Vector, dJdU0_L::FTField{n}, dJdU0_R::FTField{n}, 
+                   ∇TLJ::Real, ∇TRJ::Real, ∇sJ::Real) where {n}
+    _checksize(out, dJdU0_L, dJdU0_R)
+    idx = 1
+    @inbounds @simd for el in dJdU0_L
+        out[idx] = el
+        idx += 1
+    end
+    @inbounds @simd for el in dJdU0_R
+        out[idx] = el
+        idx += 1
+    end
+    out[end-2] = ∇TLJ
+    out[end-1] = ∇TRJ
+    out[end]   = ∇sJ
+    return out
+end
+
+tovector(dJdU0_L::FTField{n}, dJdU0_R::FTField{n}, 
+         ∇TLJ::Real, ∇TRJ::Real, ∇sJ::Real) where {n} =
+    tovector!(zeros(2*length(dJdU0_L) + 3), dJdU0_L, dJdU0_R, ∇TLJ, ∇TRJ, ∇sJ)
+
+function fromvector!(dJdU0_L::FTField{n}, dJdU0_R::FTField{n}, out::Vector) where {n}
+    _checksize(out, dJdU0_L, dJdU0_R)
+    idx = 1
+    @inbounds @simd for j in 1:length(dJdU0_L)
+        dJdU0_L[j] = out[idx]
+        idx += 1
+    end
+    @inbounds @simd for j in 1:length(dJdU0_R)
+        dJdU0_R[j] = out[idx]
+        idx += 1
+    end
+    return dJdU0_L, dJdU0_R, out[end-2], out[end-1], out[end]
+end
