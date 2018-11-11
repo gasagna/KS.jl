6f440c1fc9e15cfc4372f43006324d8464a947a8
Davide Lasagna <lasagnadavide@gmail.com>
2018-11-11 22:58:11 +0000

    use arguments in linearised equations more consistently

--- src/linear.jl
+++ src/linear.jl
@@ -24,46 +24,27 @@ LinearisedExTerm(n::Int, ISODD::Bool, mode::M) where {M <: AbstractLinearMode} =
 
 
 # evaluate linear operator around U
-function (lks::LinearisedExTerm{n, M})(t::Real,
-                                       U::FTField{n},
-                                       V::FTField{n},
-                                       dVdt::FTField{n},
-                                       add::Bool=false) where {n, M}
+function (lks::LinearisedExTerm{n, TangentMode})(t::Real,
+                                                 U::FTField{n},
+                                              dUdt::FTField{n},
+                                                 V::FTField{n},
+                                              dVdt::FTField{n},
+                                               add::Bool=false) where {n}
 
-    if M <: TangentMode
-        # /// calculate - v * uₓ term ///
-        lks.ifft(V, lks.tmp1)               # transform V to physical space
-        ddx!(lks.TMP1, U)                   # differentiate U
-        lks.ifft(lks.TMP1, lks.tmp2)        # transform Uₓ to physical space
-        lks.tmp3 .= .- lks.tmp1 .* lks.tmp2 # multiply in physical space
+    # /// calculate - v * uₓ term ///
+    lks.ifft(V, lks.tmp1)               # transform V to physical space
+    ddx!(lks.TMP1, U)                   # differentiate U
+    lks.ifft(lks.TMP1, lks.tmp2)        # transform Uₓ to physical space
+    lks.tmp3 .= .- lks.tmp1 .* lks.tmp2 # multiply in physical space
 
-        # /// calculate - u * vₓ term ///
-        lks.ifft(U, lks.tmp1)               # transform U to physical space
-        ddx!(lks.TMP2, V)                   # differentiate V
-        lks.ifft(lks.TMP2, lks.tmp2)        # transform V to physical space
-        lks.tmp3 .-= lks.tmp1 .* lks.tmp2   # multiply
+    # /// calculate - u * vₓ term ///
+    lks.ifft(U, lks.tmp1)               # transform U to physical space
+    ddx!(lks.TMP2, V)                   # differentiate V
+    lks.ifft(lks.TMP2, lks.tmp2)        # transform V to physical space
+    lks.tmp3 .-= lks.tmp1 .* lks.tmp2   # multiply
 
-        # transform sum to fourier space
-        lks.fft(lks.tmp3, lks.TMP2)
-    end
-
-    if M <: AdjointMode
-        # /// calculate - v * uₓ term ///
-        lks.ifft(V, lks.tmp1)               # transform V to physical space
-        ddx!(lks.TMP1, U)                   # differentiate U
-        lks.ifft(lks.TMP1, lks.tmp2)        # transform Uₓ to physical space
-        lks.tmp3 .= lks.tmp1 .* lks.tmp2    # multiply in physical space
-        lks.fft(lks.tmp3, lks.TMP1)         # transform product to fourier space
-
-        # /// calculate + (u*v)ₓ term ///
-        lks.ifft(U, lks.tmp2)               # transform U to physical space
-        lks.tmp2 .= lks.tmp1 .* lks.tmp2    # multiply in physical space
-        lks.fft(lks.tmp2, lks.TMP2)         # transform product to fourier space
-        ddx!(lks.TMP2)                      # differentiate in place
-
-        # sum the two terms
-        lks.TMP2 .-= lks.TMP1
-    end
+    # transform sum to fourier space
+    lks.fft(lks.tmp3, lks.TMP2)
 
     # /// store and set symmetry ///
     add == true ? (dVdt .+= lks.TMP2) : (dVdt .= lks.TMP2)
@@ -71,6 +52,37 @@ function (lks::LinearisedExTerm{n, M})(t::Real,
 
     return dVdt
 end
+
+# evaluate adjoint operator around U
+function (lks::LinearisedExTerm{n, AdjointMode})(t::Real,
+                                                 U::FTField{n},
+                                                 V::FTField{n},
+                                              dVdt::FTField{n},
+                                               add::Bool=false) where {n}
+
+    # /// calculate - v * uₓ term ///
+    lks.ifft(V, lks.tmp1)               # transform V to physical space
+    ddx!(lks.TMP1, U)                   # differentiate U
+    lks.ifft(lks.TMP1, lks.tmp2)        # transform Uₓ to physical space
+    lks.tmp3 .= lks.tmp1 .* lks.tmp2    # multiply in physical space
+    lks.fft(lks.tmp3, lks.TMP1)         # transform product to fourier space
+
+    # /// calculate + (u*v)ₓ term ///
+    lks.ifft(U, lks.tmp2)               # transform U to physical space
+    lks.tmp2 .= lks.tmp1 .* lks.tmp2    # multiply in physical space
+    lks.fft(lks.tmp2, lks.TMP2)         # transform product to fourier space
+    ddx!(lks.TMP2)                      # differentiate in place
+
+    # sum the two terms
+    lks.TMP2 .-= lks.TMP1
+
+    # /// store and set symmetry ///
+    add == true ? (dVdt .+= lks.TMP2) : (dVdt .= lks.TMP2)
+    _set_symmetry!(dVdt)
+
+    return dVdt
+end
+
 
 # ~~~ SOLVER OBJECT FOR THE LINEARISED EQUATIONS ~~~
 mutable struct LinearisedEquation{n,
@@ -111,7 +123,7 @@ function splitexim(eq::LinearisedEquation{n, IT, ET, F}) where {n, IT, ET, F<:Ab
                      V::AbstractFTField{n},
                      dVdt::AbstractFTField{n},
                      add::Bool=false)
-        eq.exTerm( t, U, V, dVdt, add)
+        eq.exTerm( t, U, dUdt, V, dVdt, add)
         eq.forcing(t, U, dUdt, V, dVdt) # note forcing always adds to dVdt
         return dVdt
     end
@@ -124,17 +136,16 @@ end
 splitexim(eq::LinearisedEquation{n, IT, ET, Nothing}) where {n, IT, ET} =
     (eq.exTerm, eq.imTerm)
 
-
 # /// EVALUATE RIGHT HAND SIDE OF LINEARISED EQUATION ///
 # Same here, if we have forcing we evaluate it, and we require passing dUdt too.
 (eq::LinearisedEquation{n, IT, ET, F})(t::Real, U, dUdt, V, dVdt) where {n, IT, ET, F<:AbstractForcing} =
-    (mul!(dVdt, eq.imTerm, V); 
-        eq.exTerm(t, U, V, dVdt, true); 
+    (mul!(dVdt, eq.imTerm, V);
+        eq.exTerm(t, U, dUdt, V, dVdt, true);
             eq.forcing(t, U, dUdt, V, dVdt); dVdt)
 
-(eq::LinearisedEquation{n, IT, ET, Nothing})(t::Real, U, V, dVdt) where {n, IT, ET} =
+(eq::LinearisedEquation{n, IT, ET, Nothing})(t::Real, U, dUdt, V, dVdt) where {n, IT, ET} =
     (mul!(dVdt, eq.imTerm, V);
-        eq.exTerm(t, U, V, dVdt, true);
+        eq.exTerm(t, U, dUdt, V, dVdt, true);
             return dVdt)
 
 # Obtain jacobian matrix (only for systems with no forcing!)
