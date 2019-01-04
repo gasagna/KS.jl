1187036bcb8dbdfbb6826f6f3478750c6dc8aeb8
Davide Lasagna <lasagnadavide@gmail.com>
2019-01-04 19:06:29 +0000

    added typing to pull eltype out

--- src/nonlinear.jl
+++ src/nonlinear.jl
@@ -44,11 +44,11 @@ Base.A_mul_B!(dUdt::AbstractFTField{n},
 
 Flows.ImcA!(imTerm::LinearTerm{n},
                  c::Real,
-                 U::AbstractFTField{n},
-              dUdt::AbstractFTField{n}) where {n} =
+                 U::AbstractFTField{n, ISODD, T},
+              dUdt::AbstractFTField{n, ISODD, T}) where {n, ISODD, T} =
     (_set_symmetry!(U);
      @inbounds for k in wavenumbers(n)
-          dUdt[k] = U[k]/(1 - c*imTerm.A[k])
+          dUdt[k] = U[k]/(one(T) - c*real(imTerm.A[k]))
      end; dUdt)
 
 
